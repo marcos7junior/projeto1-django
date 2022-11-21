9c67808fd9e93cbb69b347653ca551e920bfca69
Marcos Paiva <marcospaivajr7@gmail.com>
2022-11-21 15:19:53 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,11 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Marcos Antonio',
     })
-
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse("HELLO DJANGO sobre 1")
