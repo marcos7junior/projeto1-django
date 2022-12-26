37f34000ad7269c8b7d57c895f163dfab37a4bcc
Marcos Paiva <marcospaivajr7@gmail.com>
2022-12-26 18:50:36 -0300

    feat/ banco de dados criado

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -8,10 +16,16 @@ class Recipe(models.Model):
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=20)
     servings = models.IntegerField()
-    servings_unit = models.IntegerField()
+    servings_unit = models.CharField(max_length=20)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
